a4a73b9f763e8bd90ea30917bfc8a0b0e0d3d64a
Ahmad Ali <aa947@students.kiron.ngo>
2019-10-06 19:28:10 +0100

    -final stage

--- src/markDown.js
+++ src/markDown.js
@@ -3,16 +3,28 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import $ from "jquery";
 
+var docs = 'Instructions:                     ' +
+ '\n > type your text in the upper box' +
+ '\n > type each expression in a seperate line' +
+'\n > use * for main header ex: *Header will give you <h1>Header</h1>' +
+'\n > use ## for h4 ex: ##header => <h4>header</h4>' +
+'\n > use $$strong$$ for strong tag ex: $$strong$$ text => <strong>text</strong>' +
+'\n > use $$italic$$ for strong tag ex: $$italic$$ text => <em>text</em>' +
+'\n > use ` for code tag ex: ˋlet x; `  => <code> let x; </code>' +
+'\n > use [] for link tag ex: [google]  => <a href="https://www.google.com">google</a>' +
+'\n > use ![] for image tag ex: [img_url]  => <img src="img_url" />';
+
 
 class MarkDown extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
      input:'',
-     output:'Ahmad'
+     output: docs
     };
    this.convert_to_html = this.convert_to_html.bind(this);
    this.handleChange = this.handleChange.bind(this);
+   this.instructions = this.instructions.bind(this);
 
 }
 
@@ -32,10 +44,25 @@ convert_to_html (){
      //   console.log(str);
         let med_str;
         if(str[0]==='*'){
-         med_str = '<h1>'+str.slice(1)+'</h1>';
+         med_str = '<h1>'+str.slice(1,-1)+'</h1>';
         }
-        if (str.slice(1,3)==='##'){
-            med_str = 'h4' + str.slice(2)+'</h4>'
+        else if (str.slice(0,2)==='##'){
+            med_str = '<h4>' + str.slice(2,-1)+'</h4>'
+        }
+        else if (str.slice(0,9)==='$$strong$$'){
+            med_str = '<strong>' + str.slice(9,-1)+'</strong>'
+        }
+        else if (str.slice(0,9)==='$$italic$$'){
+            med_str = '<em>' + str.slice(9,-1)+'</em>'
+        }
+        else if (str[0]==='`'){
+            med_str = '<code>' + str.slice(1,-2)+'<code>'
+        }
+        else if (str[0]==='['){
+            med_str = '<a href="https://www.' +str.slice(1,-1)+'.com">'+str.slice(1,-1)+'</a>'
+        }
+        else if (str.slice(0,2)==='!['){
+            med_str = '<img src="'+str.slice(2,-2)+'"/>'
         }
         else {
             med_str = str;
@@ -53,7 +80,13 @@ convert_to_html (){
 
 
   return this.setState({
-        output: new_output
+        output: ''
+    });
+}
+
+instructions(){
+    return this.setState({
+        output: docs
     });
 }
 
@@ -64,18 +97,15 @@ convert_to_html (){
            <h1> MarkDown Previewer </h1>
            <h2> Editor </h2>
            <textarea  rows="6" cols="100" value={this.state.input} onChange={this.handleChange}>  </textarea>
-           <button className='btn btn-primary' style={{margin:'1% 1%'}} onClick={this.convert_to_html}> Convert to Html </button>
+           <button className='btn btn-primary' style={{margin:'2% 2%'}} onClick={this.convert_to_html}> Convert to Html </button>
+           <button className='btn btn-primary' style={{margin:'2% 2%'}} onClick={this.instructions}> Instructions </button>
            <h2>Output</h2>
-           <div id="out"> {this.state.output}   </div>
+           <div id="out"> <pre>{this.state.output}</pre>   </div>
        </div>
    );
   }
 };
  
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
 
 
 export default MarkDown;
